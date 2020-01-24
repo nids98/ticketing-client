a9fffbfc4b2f6c269e83841d6539a012ab4f0b07
Parth Patel <pmpp1920@Sahil.local>
2020-01-24 14:35:56 +0530

    get

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,60 @@
 <template>
   <div id="app">
-    <Dashboard />
+    <img alt="Vue logo" src="./assets/OYO.png" width=100px>
+    <br/>
+    <input type="text" required value="Enter Technician_id">
+    <!-- <button class="btn">Submit</button> -->
+    <input type="button" class="button button1" value="Submit">
+    <br/>
+    
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <getList/>
   </div>
 </template>
 
 <script>
-import Dashboard from "./components/Dashboard";
-
+//import HelloWorld from './components/HelloWorld.vue'
+import getList from './components/getList.vue'
 export default {
-  name: "app",
+  name: 'app',
   components: {
-    Dashboard
+    getList
   }
-};
+}
 </script>
 
-<style>
+<style scoped>
 #app {
-  font-family: "Lato", Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
+.button {
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  /* padding: 16px 32px; */
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  /* margin: 4px 2px; */
+  -webkit-transition-duration: 0.4s; /* Safari */
+  transition-duration: 0.4s;
+  cursor: pointer;
+}
+.button1 {
+  background-color: white; 
+  color: black; 
+  border: 2px solid #4CAF50;
+}
+
+.button1:hover {
+  background-color: #4CAF50;
+  color: white;
+}
+
 </style>
---
+++ src/components/getList.vue
@@ -0,0 +1,62 @@
+<template>
+  <a-table
+    :columns="columns"
+    :dataSource="data"
+    :pagination="{ pageSize: 50 }"
+    :scroll="{ y: 240 }"
+  />
+</template>
+<script>
+import axios from 'axios';
+  const columns = [
+    {
+      title: 'Task_id',
+      dataIndex: 'task_id',
+      width: 150,
+    },
+    {
+      title: 'Subcategory_id',
+      dataIndex: 'subcategory_id',
+      width: 150,
+    },
+    {
+      title: 'Status',
+      dataIndex: 'status',
+      width:150,
+    },
+    {
+      title: 'Created_at',
+      dataIndex: 'created_at',
+      width:150,
+    },
+
+  ];
+
+  const data = [];
+// //   for (let i = 0; i < 100; i++) {
+// //     data.push({
+// //       key: i,
+// //       task_id: `${i}`,
+// //       subcategory_id: 32,
+// //       status: `London, Park Lane no. ${i}`,
+// //       created_at:'2020-02-02',
+// //     });
+// //   }
+
+  export default {
+    data() {
+      return {
+        data,
+        columns,
+      };
+    },
+    mounted(){
+     axios
+     .get('http://127.0.0.1:8003/api/task/4')
+     .then(response => (this.data =response))
+     }
+    
+};
+ 
+
+</script>
